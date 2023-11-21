78a5379ef3971533f8b1414037702316299fa6ce
Daniel <148085468+daniel-yxz@users.noreply.github.com>
2023-11-21 03:05:42 -0300

    Add files via upload

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -27,31 +27,23 @@ $(document).ready(function(){
                 email: {
                     required: true,
                     email: true
-                },
-                message: {
-                    required: true,
-                    minlength: 20
                 }
             },
             messages: {
                 name: {
-                    required: "come on, you have a name, don't you?",
-                    minlength: "your name must consist of at least 2 characters"
+                    required: "Este campo é obrigatório!",
+                    minlength: "Seu nome deve possuir pelo menos 2 caracteres"
                 },
                 subject: {
-                    required: "come on, you have a subject, don't you?",
-                    minlength: "your subject must consist of at least 4 characters"
+                    required: "Este campo é obrigatório!",
+                    minlength: "Deve conter pelo menos 9 caracteres"
                 },
                 number: {
-                    required: "come on, you have a number, don't you?",
+                    required: "Este campo é obrigatório!",
                     minlength: "your Number must consist of at least 5 characters"
                 },
                 email: {
-                    required: "no email, no message"
-                },
-                message: {
-                    required: "um...yea, you have to write something to send this form.",
-                    minlength: "thats all? really?"
+                    required: "Este campo é obrigatório!"
                 }
             },
             submitHandler: function(form) {
